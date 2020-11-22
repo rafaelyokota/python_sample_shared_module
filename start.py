6738a48f841196d61ded2d71fb8834e2692ecc15
rafael yokota <rafael.yokota@ftdata.com.br>
2020-11-22 20:37:27 -0300

    args dinamic imports

--- start.py
+++ start.py
@@ -1,3 +1,28 @@
-from python_sample_shared_module_base.foo.bar import foo 
-print("Start")
-b = foo()
+# from base.foo.bar import foo 
+# print("Start")
+# b = foo()
+
+
+
+
+#!/usr/bin/env python3
+
+import sys
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--process', type=str, required=True)
+args = parser.parse_args()
+
+processName = args.process
+
+if processName == "foo":
+    from projects.project1.start import foo
+elif processName == "bar":
+    from projects.project2.start import foo2
+    
+else:
+    print("invalida process", flush=True)
+    sys.exit(1)
+    
